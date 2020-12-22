af45509258993b54814ebff08db3938a1675db26
xinwei.liu <xinwei.liu@kuwo.cn>
2020-12-23 01:14:06 +0800

    feat: 修改配置

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ userInfo.username }}</div>
+    <div class="dashboard-text">
+      <img src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80" class="user-avatar">
+      <span>name: {{ userInfo.username }}</span>
+    </div>
+    <div>
+      <img :src="emptyGif" class="emptyGif">
+    </div>
   </div>
 </template>
 
@@ -13,18 +19,37 @@ export default {
     ...mapGetters([
       'userInfo'
     ])
+  },
+  data() {
+    return {
+      emptyGif: 'https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3'
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.emptyGif {
+  display: block;
+  width: 45%;
+  margin: 0 auto;
+}
+
 .dashboard {
   &-container {
-    margin: 30px;
+    margin: 16px;
+    background-color: #e3e3e3;
   }
   &-text {
-    font-size: 30px;
+    font-size: 24px;
     line-height: 46px;
+    display: flex;
+    align-items: center;
+    padding: 16px 0 0 16px;
+      .user-avatar {
+      border-radius: 50%;
+      margin-right: 10px;
+    }
   }
 }
 </style>
